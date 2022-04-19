39c3ecf46800e61c0e2ee547071ee5fae2e7f5cd
Pavel Čácha <pavelcacha@email.cz>
2022-04-19 15:41:30 +0200

    port change to 80

--- src/shared/sharedConstants.js
+++ src/shared/sharedConstants.js
@@ -1,15 +1,19 @@
 /**
+ * server root url
+ */
+const SERVER_ROOT_URL = "http://localhost:80/";
+/**
  * institution images url
  */
-export const INSTITUTIONS_IMAGES_URL = "http://localhost:8080/institutions_images/";
+export const INSTITUTIONS_IMAGES_URL = SERVER_ROOT_URL + "institutions_images/";
 /**
  * exhibits images url
  */
-export const EXHIBITS_IMAGES_URL = "http://localhost:8080/exhibits_images/";
+export const EXHIBITS_IMAGES_URL = SERVER_ROOT_URL + "exhibits_images/";
 /**
  * info labels images url
  */
-export const INFO_LABELS_IMAGES_URL = "http://localhost:8080/info_labels_images/";
+export const INFO_LABELS_IMAGES_URL = SERVER_ROOT_URL + "info_labels_images/";
 /**
  * label showed in select boxes when nothing is selected
  */
