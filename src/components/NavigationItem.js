3a9731b6e8b17dfa3abdacc9206651da864cc129
Pavel Čácha <pavelcacha@email.cz>
2022-04-23 00:39:04 +0200

    navigation active underscore refactor

--- src/components/NavigationItem.js
+++ src/components/NavigationItem.js
@@ -7,7 +7,7 @@ import {NavLink} from "react-router-dom";
  */
 const navigationItem = (props) => (
     <li className="nav-item">
-        <NavLink exact activeClassName='active' className="nav-link" to={props.to}>
+        <NavLink exact={props.exact} activeClassName='active' className="nav-link" to={props.to}>
             {props.children}
         </NavLink>
     </li>
