3cc473e06410ec2c3312fde54e52de57da0a8335
Pavel Čácha <pavelcacha@email.cz>
2022-04-23 00:32:45 +0200

    home page cards color refactor

--- src/components/ThemesCard.js
+++ src/components/ThemesCard.js
@@ -6,9 +6,9 @@ import React from "react";
  */
 const themesCard = (props) => (
     <div className="col mt-2">
-        <div className={"my-card border rounded"}>
+        <div className="my-card border rounded">
             <h3>{props.title}</h3>
-            <p>{props.info}</p>
+            <p className="font-weight-bold">{props.info}</p>
         </div>
     </div>
 );
